53114247ee5dbe4c3ce99cb5f675166888556b15
Kushal Bansal <kushalbansal@Kushals-Laptop.local>
2024-09-10 14:40:20 +0545

    cylindrical frame comleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import { OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-  import Scene from './Scene'
-
-
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import Scene from "./Scene";
+import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
 
 const App = () => {
-  
   return (
-          <Canvas camera={{fov:35}}>
-            <OrbitControls/>
-            <ambientLight/> 
-            <Scene/>
-           
-      
-      
+    <>
+    <Canvas  flat camera={{ fov: 35 }}>
+      {/* <OrbitControls /> */}
+      <ambientLight />
+      <Scene />
+      <EffectComposer>
+        <Bloom
+        mipmapBlur
+        intensity={12.0} // The bloom intensity.
+        luminanceThreshold={0.36} // luminance threshold. Raise this value to mask out darker elements in the scene.
+        luminanceSmoothing={0.2} // smoothness of the luminance threshold. Range is [0, 1]
+        />
+        {/* <ToneMapping adaptive /> */}
+      </EffectComposer>
     </Canvas>
-  )
-}
+    <div className="w-full bg-zinc-900 py-32 text-white">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt non debitis aut dignissimos facilis! Vel, culpa maxime temporibus doloribus eos minus, nulla exercitationem mollitia molestias amet reiciendis sapiente, quas consectetur voluptatibus quasi odio. Aperiam ad obcaecati praesentium, earum porro inventore?</h1>
+    </div>
+        </>
+  );
+};
 
-export default App
+export default App;
