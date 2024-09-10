2bea9eebef7c54541c8ecc8d79a48be94c1b7de1
Kushal Bansal <kushalbansal@Kushals-Laptop.local>
2024-09-10 14:00:09 +0545

    cylinder created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import * as THREE from "three"
+
 
 const App = () => {
   return (
-    <div>
+          <Canvas>
+            <OrbitControls/>
+            <ambientLight/> 
+            <mesh>
+            {/* <cylinderGeometry /> */}
+            <cylinderGeometry args={[1,1,1,30,30,true]}/>
+            
+            <meshStandardMaterial side={THREE.DoubleSide}/>
+            </mesh>
       
       
-    </div>
+    </Canvas>
   )
 }
 
